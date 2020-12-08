54382337f0841622b3d4990678ade0cbd207ea2e
Thomas Clausen <kontakt@thomasclausen.dk>
2020-12-08 10:38:57 +0100

    Update thomasclausen.js

--- _data/sites/thomasclausen.js
+++ _data/sites/thomasclausen.js
@@ -1,7 +1,6 @@
 module.exports = {
   name: "thomasclausen.dk", // optional, falls back to object key
   description: "Statistics for my own site",
-  skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
   options: {
     frequency: 60 * 23, // (in minutes), 23 hours
   },
