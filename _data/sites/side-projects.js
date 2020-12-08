1bcfee726fe79c73b5c1b76099de5109f4beae70
Thomas Clausen <kontakt@thomasclausen.dk>
2020-12-08 10:40:17 +0100

    Update side-projects.js

--- _data/sites/side-projects.js
+++ _data/sites/side-projects.js
@@ -1,7 +1,6 @@
 module.exports = {
   name: "side-projects", // optional, falls back to object key
   description: "Statistics for my silly side projects",
-  skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
   options: {
     frequency: 60 * 23, // (in minutes), 23 hours
     freshChrome: "run"
